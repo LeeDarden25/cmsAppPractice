a70d6a9a075d3ae9be4ccb5184e86d1ad7871512
unknown <LeeDarden28@icloud.com>
2025-04-05 00:07:30 -0400

    clone

---
+++ api/WebApplication1/WebApplication1/Controllers/cmsAppController.cs
@@ -0,0 +1,93 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class cmsAppController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public cmsAppController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetCustomers")]
+        public JsonResult GetCustomers()
+        {
+
+            string query = "select * from dbo.Customers";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("cmsAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon=new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        [Route("AddCustomer")]
+        public JsonResult AddCustomer([FromForm] string newCustomer , [FromForm] string newCustomerEmail , [FromForm] string newCustomerPhone)
+        {
+
+            string query = "insert into dbo.Customers(name, email, phone) values(@newCustomer, @newCustomerEmail, @newCustomerPhone) ";
+
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("cmsAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@newCustomer", newCustomer);
+                    myCommand.Parameters.AddWithValue("@newCustomerEmail", newCustomerEmail);
+                    myCommand.Parameters.AddWithValue("@newCustomerPhone", newCustomerPhone);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Customer Successfully");
+        }
+        [HttpDelete]
+        [Route("DeleteCustomer")]
+        public JsonResult DeleteCustomer(int id)
+        {
+
+            string query = "delete from dbo.Customers where id=@id";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("cmsAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Customer Successfully");
+        }
+    }
+}
